b51a4d76ece7e4b1372d333d2961df3679ada0ec
santhiravi03 <santhiravi03@gmail.com>
2025-03-14 12:09:08 +0530

    Update Analyticalfunctions.sql

--- SQLQueries/Analyticalfunctions.sql
+++ SQLQueries/Analyticalfunctions.sql
@@ -1,1 +1,11 @@
+-- 6. Assign a rank to employees based on their salary within each department.
+-- This query assigns the ranking to the complete data as per the values in the column salary starting rank 1 with the highest salary
+SELECT department_id, employee_id, salary, 
+       RANK() OVER (ORDER BY salary DESC) AS salary_rank
+FROM HR.employees;
 
+-- the below query groups each department id and then applies the ranking by salary only within each group of department
+
+SELECT department_id, employee_id, salary, 
+       RANK() OVER (Partition by department_id ORDER BY salary DESC) AS salary_rank
+FROM HR.employees;
