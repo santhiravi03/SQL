2a5f977cf124562ebde6ae6c348bd1e4150e4308
santhiravi03 <santhiravi03@gmail.com>
2025-03-14 10:24:55 +0530

    Update Aggregations.sql

--- SQLQueries/Aggregations.sql
+++ SQLQueries/Aggregations.sql
@@ -1,1 +1,5 @@
-
+-- 1. Count the number of employees in each department
+-- This query groups employees by department_id and counts how many employees exist in each.
+SELECT department_id, COUNT(*) AS total_employees
+FROM hr.employees
+GROUP BY department_id;
