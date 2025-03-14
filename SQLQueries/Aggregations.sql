24edd14a6d12792fc3b9e11dabdde04584b89062
santhiravi03 <santhiravi03@gmail.com>
2025-03-14 10:34:36 +0530

    Update Aggregations.sql

--- SQLQueries/Aggregations.sql
+++ SQLQueries/Aggregations.sql
@@ -3,3 +3,10 @@
 SELECT department_id, COUNT(*) AS total_employees
 FROM hr.employees
 GROUP BY department_id;
+
+-- 2. Find the average salary in each department
+-- this query finds the average for each of the department by grouping at the department id level
+select DEPARTMENT_ID,avg(SALARY) from hr.employees
+group by DEPARTMENT_ID
+
+
